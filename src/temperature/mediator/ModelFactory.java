5b4cfb25ca51959b73df7a890e18308320af61c0
Abdullahi <abdullahi12351@gmail.com>
2021-02-25 12:18:22 +0100

    Initial commit

---
+++ src/temperature/mediator/ModelFactory.java
@@ -0,0 +1,14 @@
+package temperature.mediator;
+
+public class ModelFactory {
+
+    private TemperatureModel temperatureModel;
+
+    public TemperatureModel getTemperatureModel() {
+        if(temperatureModel == null) {
+            temperatureModel = new TemperatureModelManager();
+        }
+        return temperatureModel;
+    }
+
+}
